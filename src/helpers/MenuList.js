af5aa09fd59f4084930fa7ff338a71e3941de6f9
Achini Karunarathna <135375586+AchiniMK@users.noreply.github.com>
2024-02-14 11:19:27 +0530

    Second Commit

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,41 @@
+
+import Pepperoni from "../assets/pepperoni.jpg";
+import Margherita from "../assets/margherita.jpg";
+import AchiniSpecial from "../assets/achinispecialpizza.jpg";
+import Vegan from "../assets/vegan.jpg";
+import Pineapple from "../assets/pineapple.jpg";
+import Expensive from "../assets/expensive.jpg";
+
+
+export const MenuList = [
+    {
+      name: "Pepperoni Pizza",
+      image: Pepperoni,
+      price: 2999,
+    },
+    {
+      name: "Margherita Pizza",
+      image: Margherita,
+      price: 2499,
+    },
+    {
+      name: "Achini's Special Pizza",
+      image: AchiniSpecial,
+      price: 3199,
+    },
+    {
+      name: "Vegan Pizza",
+      image: Vegan,
+      price: 2599,
+    },
+    {
+      name: "Pineapple Pizza",
+      image: Pineapple,
+      price: 1999,
+    },
+    {
+      name: "Very Expensive Pizza",
+      image: Expensive,
+      price: 5999,
+    },
+  ];
